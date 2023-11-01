4171516e6d350eb7c5118d7f8c829e7f03caa556
Pratap Rajput <95515165+prataprajput27@users.noreply.github.com>
2023-11-01 14:05:41 +0530

    about to merge

--- client/src/components/Blog/Blogs/Blogs.js
+++ client/src/components/Blog/Blogs/Blogs.js
@@ -8,72 +8,76 @@ import { format } from "timeago.js";
 import Footer from "../../Home/Footer/Footer";
 import Header from "../../Share/Header/Header";
 import "./Blogs.css";
-
+import axios from "../../../utils/axiosConfig";
+import { useEffect, useState } from "react";
 const Blogs = () => {
-  const blogCollection = {};
-
+  const [blogCollection, setBlogCollection] = useState([]);
+  useEffect(() => {
+    const getAllBlog = async () => {
+      const res = await axios.get("/api/Blog");
+      console.log("Res", res.data);
+      setBlogCollection(res.data);
+    };
+    getAllBlog();
+  }, []);
+  console.log("blogs", blogCollection);
   return (
     <>
       <Header />
       <Container className="mb-5 pb-5">
         <div className="blog-tle pb-5">
           <div class="one">
-            <h1 style={{ color: "#D14836" }} className="re">
-              Our Recent Article
-            </h1>
+            <h1 className="re">Our Recent Article</h1>
           </div>
         </div>
         <Row className="blog-my gx-4 gy-5" xs={1} md={2} lg={3} xl={4}>
-          {(
-            blogCollection?.map((blog) => (
-              <Col>
-                <div className="card-design h-100">
-                  <div className="img-design">
-                    <img
-                      className="img-fluid"
-                      src={`data:image/*;base64,${blog?.photo}`}
-                      alt=""
-                    />
-                    <p data-tag="Total View" className="view-div">
-                      <span className="view">
-                        <MdVisibility /> {blog?.totalVisitor.length}
-                      </span>
-                    </p>
-                    <p data-tag="Total Love" className="love-div">
-                      <span className="love">
-                        <BsFillSuitHeartFill /> {blog?.likes.length}
-                      </span>
-                    </p>
-                  </div>
-                  <div className="service-card">
-                    <Link
-                      data-tag={blog?.title}
-                      className="title-heading text-decoration-none"
-                      to={`/blog/${blog?._id}`}
-                    >
-                      <h2>{blog?.title}</h2>
-                    </Link>
-                    <p className="blog-writer">
-                      Published By <span>Admin {format(blog?.date)}</span>{" "}
-                    </p>
-                    <br />
-                    <p className="blog-des">{blog?.description}</p>
-                    <Link
-                      className="custom-btn btn-9 text-decoration-none "
-                      to={`/blog/${blog?._id}`}
-                    >
-                      Read More
-                    </Link>
-                  </div>
+          {blogCollection?.map((blog) => (
+            <Col>
+              <div className="card-design h-100">
+                <div className="img-design">
+                  <img
+                    className="img-fluid"
+                    src={`data:image/*;base64,${blog?.photo}`}
+                    alt=""
+                  />
+                  <p data-tag="Total View" className="view-div">
+                    <span className="view">
+                      <MdVisibility /> {blog?.totalVisitor.length}
+                    </span>
+                  </p>
+                  <p data-tag="Total Love" className="love-div">
+                    <span className="love">
+                      <BsFillSuitHeartFill /> {blog?.likes.length}
+                    </span>
+                  </p>
                 </div>
-              </Col>
-            ))
-          )}
+                <div className="service-card">
+                  <Link
+                    data-tag={blog?.title}
+                    className="title-heading text-decoration-none"
+                    to={`/blog/${blog?._id}`}
+                  >
+                    <h2>{blog?.title}</h2>
+                  </Link>
+                  <p className="blog-writer">
+                    Published By <span>Admin {format(blog?.date)}</span>{" "}
+                  </p>
+                  <br />
+                  <p className="blog-des">{blog?.description}</p>
+                  <Link
+                    className="custom-btn btn-9 text-decoration-none "
+                    to={`/blog/${blog?._id}`}
+                  >
+                    Read More
+                  </Link>
+                </div>
+              </div>
+            </Col>
+          ))}
         </Row>
       </Container>
       <Footer />
     </>
   );
 };
-
 export default Blogs;

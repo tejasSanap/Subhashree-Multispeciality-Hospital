a4159ab6de61d77555281180f77eaee8b2fef900
Varad Rajopadhye <varadmanraj@gmail.com>
2023-12-14 19:14:30 +0530

    Added Logging for ELK , changed env

--- server/app.js
+++ server/app.js
@@ -8,8 +8,20 @@ const blogRoute = require("../server/routes/blogRoutes");
 const reviewRoute = require("./routes/reviewRoutes");
 const hospitalRoute = require("./routes/hospitalRoute");
 const appointmentRoute = require("./routes/appointmentRoute");
-const authAdminRoute = require("../server/routes/authAdmin")
-const { authenticate } = require("./middleware/auth.middleware");
+const authAdminRoute = require("../server/routes/authAdmin");
+const expressWinston = require("express-winston");
+const winston = require("winston");
+const DailyRotateFile = require("winston-daily-rotate-file");
+
+const fileRotateTransport = new DailyRotateFile({
+  filename: "logs/rotate-%DATE%.log",
+  datePattern: "YYYY-MM-DD",
+  maxFiles: "8d",
+  maxSize: "10m",
+});
+
+expressWinston.requestWhitelist.push("body");
+expressWinston.responseWhitelist.push("body");
 
 require("dotenv").config();
 //env variables
@@ -25,6 +37,90 @@ async function connectToMongoDB() {
     console.log("error connect to db", err);
   }
 }
+
+app.use(
+  expressWinston.logger({
+    transports: [fileRotateTransport, new winston.transports.Console()],
+    meta: true,
+    dynamicMeta: (req, res) => {
+      const httpRequest = {
+        requestMethod: req.method,
+        requestUrl: `${req.protocol}://${req.get("host")}${req.originalUrl}`,
+        protocol: `HTTP/${req.httpVersion}`,
+        remoteIp:
+          req.ip.indexOf(":") >= 0
+            ? req.ip.substring(req.ip.lastIndexOf(":") + 1)
+            : req.ip,
+        requestSize: req.socket.bytesRead,
+        userAgent: req.get("User-Agent"),
+        referrer: req.get("Referrer"),
+        requestPath: req.originalUrl.split("?")[0],
+      };
+
+      let meta = {
+        httpRequest,
+      };
+
+      if (req.body) {
+        if (res.statusCode >= 500 && res.statusCode <= 599) {
+          meta.req = {
+            body: req.body,
+            headers: req.headers,
+            query: req.query,
+          };
+        } else {
+          meta.req = {
+            body: null,
+            headers: req.headers,
+            query: req.query,
+          };
+        }
+      }
+
+      meta.timestamp = new Date().toISOString();
+      meta.serverType = "hospital";
+      meta.projectName = "subhashree-multispeciality-hospital";
+      meta.serverConfigType = process.env.SERVER_CONFIG_TYPE;
+      meta.responseTime = res.responseTime;
+      meta.userType = req.headers.usertype;
+      meta.userId = req.headers.userid;
+
+      if (res) {
+        httpRequest.status = res.statusCode;
+        httpRequest.latency = {
+          seconds: Math.floor(res.responseTime / 1000),
+          nanos: (res.responseTime % 1000) * 1000000,
+        };
+
+        if (res.body) {
+          if (typeof res.body === "object") {
+            httpRequest.responseSize = JSON.stringify(res.body).length;
+          } else if (typeof res.body === "string") {
+            httpRequest.responseSize = res.body.length;
+          }
+          if (res.statusCode < 500 || res.statusCode > 599) {
+            meta.res = {
+              body: null,
+            };
+          } else
+            meta.res = {
+              body: res.body,
+            };
+        }
+      }
+
+      return meta;
+    },
+    colorize: true,
+    ignoreRoute: function (req, res) {
+      if (req.method === "OPTIONS") {
+        return true;
+      }
+      return false;
+    },
+  })
+);
+
 connectToMongoDB();
 
 app.use(cors());
@@ -38,7 +134,6 @@ app.use("/api", reviewRoute);
 app.use("/api", blogRoute);
 app.use("/api/hospital", hospitalRoute);
 app.use("/api/appointment", appointmentRoute);
-
 
 app.get("/api/dashboard", (req, res, next) => {
   res.json("this was protected route");

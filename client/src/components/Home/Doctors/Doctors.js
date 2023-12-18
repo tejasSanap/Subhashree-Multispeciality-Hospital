c43a087ed4c21d6bd9b7283ae71ade89c0631ce6
Pratap Rajput <95515165+prataprajput27@users.noreply.github.com>
2023-12-19 03:15:15 +0530

    fix/doctor-hover

--- client/src/components/Home/Doctors/Doctors.js
+++ client/src/components/Home/Doctors/Doctors.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import './Doctor.css';
-import DoctorsSlider from './DoctorSlide/DoctorsSlider';
+import React, { useEffect, useState } from "react";
+import "./Doctor.css";
+import DoctorsSlider from "./DoctorSlide/DoctorsSlider";
 
 const Doctors = () => {
   const [pageDoctors, setPageDoctors] = useState(0);
   const handleHight = () => setPageDoctors(window.pageYOffset);
   useEffect(() => {
-    window.addEventListener('scroll', handleHight);
+    window.addEventListener("scroll", handleHight);
   }, []);
   return (
     <div className="doctors">
@@ -14,9 +14,10 @@ const Doctors = () => {
         OUR DOCTORS
       </h3>
       <div className="doctor-section text-center">
-        <h5 style={{ letterSpacing: '3px' }}>Doctors</h5>
-        <h1>
-          Our outstanding doctors <br /> <span> are active to help you!</span>
+        <h5 style={{ letterSpacing: "3px" }}>Doctors</h5>
+        <h1 style={{ fontWeight: "600" }}>
+          Our Outstanding Doctors <br />
+          <span style={{ fontSize: "24px" }}> are active to help you!</span>
         </h1>
         <DoctorsSlider />
       </div>
